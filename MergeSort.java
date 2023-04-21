6cd6d071a1a47fc158f62670a8c9de7ce9ec8d4f
SamedBilgin <131148590+SamedBilgin@users.noreply.github.com>
2023-04-21 22:31:13 +0300

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,5 +1,8 @@
 public class MergeSort {
-     // Here we are checking the array.If its empty function returns. 
+     
+    // The main function that calls mergeSort to sort the given array.
+    // It checks if the array is empty or null before proceeding.
+     
     public void sort(int[] arr) {
         if (arr == null || arr.length == 0) {
             return;
@@ -7,8 +10,10 @@ public class MergeSort {
         mergeSort(arr, 0, arr.length - 1);
     }
      
-    // mergeSort firstly divides the array into atomic parts then start merging them with merge method with correct order.
-    
+    // Merge sort function that divides the array into atomic parts 
+    // and recursively sorts them by calling itself.
+    // It then merges the sorted subarrays using the merge() function.
+     
     private void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2;
@@ -18,12 +23,18 @@ public class MergeSort {
         }
     }
 
+    // The merge function that merges two sorted subarrays into a single sorted subarray.
+    // It creates two subarrays from the given array by dividing it at the midpoint.
+    // It then iterates over both subarrays, compares the elements and adds the smaller element to the result array.
+    // Finally, it adds the remaining elements to the result array, if any.
+     
     private void merge(int[] arr, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
         int[] leftArr = new int[n1];
         int[] rightArr = new int[n2];
 
+        // Create two subarrays from the original array by dividing it at the midpoint
         for (int i = 0; i < n1; i++) {
             leftArr[i] = arr[left + i];
         }
@@ -31,6 +42,7 @@ public class MergeSort {
             rightArr[j] = arr[mid + 1 + j];
         }
 
+        // Merge the two subarrays by comparing the elements and adding the smaller element to the result array
         int i = 0, j = 0;
         int k = left;
         while (i < n1 && j < n2) {
@@ -40,6 +52,9 @@ public class MergeSort {
                 arr[k++] = rightArr[j++];
             }
         }
+
+        // Add the remaining elements to the result array, if any
+         
         while (i < n1) {
             arr[k++] = leftArr[i++];
         }
@@ -47,4 +62,3 @@ public class MergeSort {
             arr[k++] = rightArr[j++];
         }
     }
-}
