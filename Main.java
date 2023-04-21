9e16ece776069f9dd6ebfd50dc65b03a5110c771
SamedBilgin <131148590+SamedBilgin@users.noreply.github.com>
2023-04-21 22:26:13 +0300

    Main.java
    Test case for merge sort algorithm.

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[]args){
+        
      int[] arr = {4, 1, 8, 3, 5};
+        
      MergeSort mergeSort = new MergeSort();
+        
      mergeSort.sort(arr);
+        
    for(int i=0;i<5;i++){
     System.out.print(arr[i] );
 
